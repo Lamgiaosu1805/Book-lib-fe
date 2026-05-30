cbf61cd2d8e26b2d66a5badc02adc268e996a562
Lâm GS <nghiemkhaclam185.mta@gmail.com>
2026-05-30 09:56:41 +0700

    redirect login when 403 401

--- lib/api.ts
+++ lib/api.ts
@@ -6,4 +6,15 @@ const api = axios.create({
   baseURL: BASE_URL,
 });
 
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (typeof window !== 'undefined' && (error.response?.status === 401 || error.response?.status === 403)) {
+      document.cookie = 'user_token=; path=/; max-age=0';
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default api;
